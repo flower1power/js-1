0d9691f63a5083f939daea604c140646bb28e6c0
flower1power <sibfight@mail.ru>
2023-08-18 13:47:01 +0300

    False из других типов

---
+++ les4_9.js
@@ -0,0 +1,14 @@
+//! False из других типов
+console.log(Boolean(0));
+console.log(Boolean(''));
+let a;
+console.log(Boolean(a));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(Number('sx')));
+console.log(Boolean(NaN));
+console.log(Number('10-1'))
+console.log(String('10') - 5 + Boolean('') + '1');
+
+
+
