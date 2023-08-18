e2cf5ee7c4e25c5716c70442484e3ab66b0600ef
flower1power <sibfight@mail.ru>
2023-08-18 14:37:25 +0300

    Switch

---
+++ les5_4.js
@@ -0,0 +1,54 @@
+//! Switch
+
+const role = 'manager';
+
+if (role === 'manager') {
+  console.log('Менеджер');
+} else if (role === 'admin') {
+  console.log('Админ');
+} else if (role === 'ceo') {
+  console.log('СЕО');
+} else {
+  console.log('Мы тебя не знаем!')
+}
+
+//? Хорошая практика замены else if
+switch (role) {
+  case 'manager':  //* role === 'manager'
+    console.log('Менеджер');
+    break
+  case 'admin':
+    console.log('Админ');
+    break
+  case 'ceo':
+    console.log('СЕО');
+    break
+  default:
+    console.log('Мы тебя не знаем!')
+}
+
+//? Кейс с обьединением действий
+switch (role) {
+  case 'manager':
+  case 'admin':
+    console.log('Не руководитель');
+    break
+  case 'ceo':
+    console.log('Руководитель');
+    break
+  default:
+    console.log('Мы тебя не знаем!')
+}
+
+const num = 1;
+
+switch(true) {
+  case num > 0: //* true === num > 0
+    console.log('Положительный')
+    break
+  case num < 0: //* true === num < 0
+    console.log('Отрицательный')
+    break
+  default:
+    console.log('Ноль!')
+}
