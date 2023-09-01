4a40affe10197d30339bbb244a39d2a778f96bf4
flower1power <sibfight@mail.ru>
2023-09-01 13:01:09 +0300

    Анонимные функции

---
+++ les7_2.js
@@ -0,0 +1,16 @@
+//! Анонимные функции
+
+function powerOfTwo(num) {
+  return num * num;
+}
+
+console.log(powerOfTwo(5));
+
+
+
+//? Функцию назначили на переменную
+const poft = function (num) {
+  return num * num;
+}
+
+console.log(poft(6));
