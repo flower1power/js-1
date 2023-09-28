012f81d0aa7655180ffc5ee70800fc5ce054d280
flower1power <sibfight@mail.ru>
2023-09-28 19:44:07 +0300

    forEach/map/filter/test

---
+++ lesFilter11_3.js
@@ -0,0 +1,27 @@
+//! filter
+
+const operations = [100, -20, 7, -20, 50];
+
+// const positiveOperations = [];
+
+// for (const operation of operations) {
+//   if (operation > 0) {
+//     positiveOperations.push(operation);
+//   }
+// }
+
+// console.log(positiveOperations);
+
+//?Оставляет все что возвращает true
+const positiveOperations = operations.filter(operation => {
+  return operation > 0;
+})
+console.log(positiveOperations);
+
+//?Использование map и filter
+const positiveRUBOperations = operations
+  .filter(operation => {
+    return operation > 0;
+  })
+  .map(operation => operation * 60);
+console.log(positiveRUBOperations);
