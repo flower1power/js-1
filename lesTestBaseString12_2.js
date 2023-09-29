b6e92bb915cbdf6f275450ec2eff5150a8180493
flower1power <sibfight@mail.ru>
2023-09-29 23:20:44 +0300

    .

--- lesTestBaseString12_2.js
+++ lesTestBaseString12_2.js
@@ -10,13 +10,13 @@
 // console.log(name);
 // console.log(surName);
 
-const fullUserName = "Вася aka Terminator Predator Пупкин";
+const fullUserName = 'Вася aka Terminator Predator Пупкин';
 
-const userName = fullUserName.slice(0, fullUserName.indexOf(" "));
+const userName = fullUserName.slice(0, fullUserName.indexOf(' '));
 console.log(userName);
 
 const userSurName = fullUserName.slice(
-  fullUserName.lastIndexOf(" ") + 1,
+  fullUserName.lastIndexOf(' ') + 1,
   fullUserName.length
 );
 console.log(userSurName);
